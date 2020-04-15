7c465f2815575868fe94315c56c6a606d79c2148
Glennichouffe <g.crompton@students.ephec.be>
2020-04-15 12:02:34 +0200

    Ajout de styled components

--- dev/frontend/src/components/NavigationBar.jsx
+++ dev/frontend/src/components/NavigationBar.jsx
@@ -3,21 +3,37 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import styled from "styled-components";
 
+const Styles = styled.div`
+  .navbar {
+    background-color: #27742d;
+  }
+
+  .navbar-nav .nav-link {
+    color: white;
+
+    &:hover {
+      color: black;
+    }
+  }
+`;
+
 export const NavigationBar = () => (
-  <Navbar expand="lg">
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Item>
-          <Nav.Link href="/home">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/announcements">Annonces</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/timeTables">Horaires</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <Styles>
+    <Navbar expand="xl">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link href="/home">Accueil</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/announcements">Annonces</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/timeTables">Horaires</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
 );
