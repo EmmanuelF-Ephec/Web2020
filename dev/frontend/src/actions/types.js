27ef3fcf0acd2b563e97591e65b81ea43ff91962
Glennichouffe <g.crompton@students.ephec.be>
2020-05-11 17:03:42 +0200

    Chnagement de placement de fichier (structure)

--- dev/frontend/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const GET_USER = "GET_USER";
-export const AUTH_START = 'AUTH-START';
-export const AUTH_SUCCESS = 'AUTH-SUCCESS';
-export const AUTH_FAIL = 'AUTH-FAIL';
-export const AUTH_LOGOUT = 'AUTH-LOGOUT';
